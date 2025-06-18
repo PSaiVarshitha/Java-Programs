373314033bd35442f918a443c995dbdcf61b0e97
PSaiVarshitha <varshi0228v@gmail.com>
2025-06-18 12:15:15 +0530

    Create simple_intrest.java

---
+++ Java Problems/simple_intrest.java
@@ -0,0 +1,12 @@
+public class simple_intrest {
+  public static void main(String args[]){
+  int p=10000;
+  int t=500;
+  int r=1000;
+  int result ;
+    result=((p*t*r)/100);
+  System.out.println(result);
+  }
+
+  
+}
