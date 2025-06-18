4b155099c2d9225975fcadd4528ef333c4505f24
PSaiVarshitha <varshi0228v@gmail.com>
2025-06-18 12:11:35 +0530

    Create celcius.java

---
+++ Java Problems/celcius.java
@@ -0,0 +1,8 @@
+public class celcius {
+  public static void main(String args[]){
+    float fahrenheit,celcius;
+    fahrenheit=43;
+    celcius=( (fahrenheit-32)*5)/9;
+    System.out.println("temp in celcius is:"+celcius);
+  }
+}
